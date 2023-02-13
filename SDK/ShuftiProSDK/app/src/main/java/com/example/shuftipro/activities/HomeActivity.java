598b923b0e08cf6213c1db2408178652cda6b814
shuftipro <support@shuftipro.com>
2023-02-13 16:23:54 +0500

    0.3.7 timeout response handled

--- SDK/ShuftiProSDK/app/src/main/java/com/example/shuftipro/activities/HomeActivity.java
+++ SDK/ShuftiProSDK/app/src/main/java/com/example/shuftipro/activities/HomeActivity.java
@@ -24,6 +24,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
